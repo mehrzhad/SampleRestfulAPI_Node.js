4ed94b4a6e04a68be73946a61cffa397684a8367
Mehran Hamedani Nezhad <mehran.hamedaninezhad@gmail.com>
2018-11-26 14:30:30 +0330

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+
+// require
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const countries     = require('./app/data/countries');
+
+// variables
+const portNumber    = 5000;
+const app           = express();
+
+// init variables
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// get api
+app.get('/getCountries', (req, res) =>{
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(countries));
+});
+
+app.get('/getCountryByCode/:code', (req, res) =>{
+    let code = req.params.code;
+    let country = countries.find(c => c.code == code);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(country));
+});
+
+// post api
+app.post('/createCountry', (req, res) =>{
+    let country = req.body;
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(country));
+});
+
+// listen
+app.listen(portNumber, () =>{
+    console.log('listen to port ' + portNumber);
+});
